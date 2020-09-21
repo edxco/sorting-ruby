77fc9b3e00be6b5e163f429cd8f3cb3b57c43788
Ricardo <ramn21@gmail.com>
2020-09-22 00:51:52 +0200

    fix sort

--- sort.rb
+++ sort.rb
@@ -1,5 +1,3 @@
-my_array = [15, 9, 7, 2, 3, 1]
-
 def bubble_sorting(arr)
   (0...arr.length).each do |j|
     (1...arr.length - j).each do |i|
@@ -12,4 +10,23 @@ def bubble_sorting(arr)
   return arr
 end
 
+def bubble_sort_by(arr)
+  arr.length.times do |i|     
+    (1...arr.length-i).each do |j|
+      element = yield(arr[j], arr[j - 1]).positive? 
+      if element == false
+        temp = arr[j] 
+        arr[j] = arr[j - 1] 
+        arr[j-1] = temp
+      end
+    end
+  end
+  return arr
+end
+
+
+my_array = [15, 9, 7, 2, 3, 1]
 print bubble_sorting(my_array)
+
+sorted = bubble_sort_by( %w('hello' 'iiiiiiii' 'hell' 'h') { | left, right | left.length - right.length })
+puts sorted
