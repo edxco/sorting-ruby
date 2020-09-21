7594ab1ac12f008d279225e60e4c65356c6cc68c
Eduardo Baeza <eduardo.baeza@yahoo.com>
2020-09-21 17:23:56 -0500

    linters

--- sort.rb
+++ sort.rb
@@ -1,15 +1,15 @@
 my_array = [15, 9, 7, 2, 3, 1]
 
 def bubble_sorting(arr)
-    for j in (0...arr.length)
-       for i in (1...arr.length-j)
-            if arr[i] < arr[i-1]
-                arr.insert(i-1, arr[i])
-                arr.delete_at(i+1)
-            end
-        end
+  (0...arr.length).each do |j|
+    (1...arr.length - j).each do |i|
+      if arr[i] < arr[i - 1]
+        arr.insert(i - 1, arr[i])
+        arr.delete_at(i + 1)
+      end
     end
-    return arr
+  end
+  return arr
 end
 
 print bubble_sorting(my_array)
